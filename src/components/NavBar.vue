7b618e188a5fc9cb1555088c624b9104e016d777
monstajoe2002 <youssef.amr90@gmail.com>
2025-06-17 18:10:59 +0300

    update NavBar links to reflect correct routes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,8 +3,8 @@
         <nav>
             <ul>
                 <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
+                <li><a href="/products">Products</a></li>
+                <li><a href="/contact">Contact Us</a></li>
             </ul>
         </nav>
     </header>
