162f1511e79bdab33be86c24733ea80e5a27d890
monstajoe2002 <youssef.amr90@gmail.com>
2025-06-17 18:18:13 +0300

    create navlink styles and apply them

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,11 +1,28 @@
 <template>
     <header>
         <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/products">Products</a></li>
-                <li><a href="/contact">Contact Us</a></li>
+            <ul class="navlinks">
+                <li><a href="/" class="navlink">Home</a></li>
+                <li><a href="/products" class="navlink">Products</a></li>
+                <li><a href="/contact" class="navlink">Contact Us</a></li>
             </ul>
         </nav>
     </header>
 </template>
+
+
+<style scoped>
+.navlinks {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    list-style: none;
+}
+
+.navlink {
+    text-decoration: none;
+    padding: 10px 15px;
+    color: black;
+    font-family: 'Poppins', sans-serif;
+}
+</style>
