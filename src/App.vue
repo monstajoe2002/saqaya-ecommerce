28aef02d4dfc6fc28adff0c3eceadb86ebfb83eb
monstajoe2002 <youssef.amr90@gmail.com>
2025-06-17 18:40:24 +0300

    feat: remove poppins classes

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@ import NavBar from './components/NavBar.vue';
 
 <template>
   <NavBar />
-  <main class="container poppins-regular">
+  <main class="container">
     <RouterView />
   </main>
 </template>
@@ -13,100 +13,14 @@ import NavBar from './components/NavBar.vue';
 <style>
 *,
 *::before,
-*::after {
+*::after,
+a {
   margin: 0;
   padding: 0;
   border: 0;
   box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
 }
-
-
-/* Poppins CSS classes */
-.poppins-light {
-  font-family: "Poppins", sans-serif;
-  font-weight: 300;
-  font-style: normal;
-}
-
-.poppins-regular {
-  font-family: "Poppins", sans-serif;
-  font-weight: 400;
-  font-style: normal;
-}
-
-.poppins-medium {
-  font-family: "Poppins", sans-serif;
-  font-weight: 500;
-  font-style: normal;
-}
-
-.poppins-semibold {
-  font-family: "Poppins", sans-serif;
-  font-weight: 600;
-  font-style: normal;
-}
-
-.poppins-bold {
-  font-family: "Poppins", sans-serif;
-  font-weight: 700;
-  font-style: normal;
-}
-
-.poppins-extrabold {
-  font-family: "Poppins", sans-serif;
-  font-weight: 800;
-  font-style: normal;
-}
-
-.poppins-black {
-  font-family: "Poppins", sans-serif;
-  font-weight: 900;
-  font-style: normal;
-}
-
-.poppins-light-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 300;
-  font-style: italic;
-}
-
-.poppins-regular-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 400;
-  font-style: italic;
-}
-
-.poppins-medium-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 500;
-  font-style: italic;
-}
-
-.poppins-semibold-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 600;
-  font-style: italic;
-}
-
-.poppins-bold-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 700;
-  font-style: italic;
-}
-
-.poppins-extrabold-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 800;
-  font-style: italic;
-}
-
-.poppins-black-italic {
-  font-family: "Poppins", sans-serif;
-  font-weight: 900;
-  font-style: italic;
-}
-
-
 
 .container {
   min-height: 100vh;
