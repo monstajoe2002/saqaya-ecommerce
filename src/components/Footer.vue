0c0e964a9a098502148547c39f2b1780e58055c2
monstajoe2002 <youssef.amr90@gmail.com>
2025-06-17 19:50:05 +0300

    fix: restructure footer template and style for better organization

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,25 @@
 <template>
     <footer class="footer">
-        <h3>Navigate</h3>
-        <ul>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Contact Us</li>
-            <li>Sitemap</li>
-        </ul>
+        <div>
+            <h3>Navigate</h3>
+            <ul class="footer-links">
+                <li>About</li>
+                <li>Blog</li>
+                <li>Contact Us</li>
+                <li>Sitemap</li>
+            </ul>
+        </div>
     </footer>
 
 </template>
 
 <style scoped>
 .footer {
+    padding: 1rem;
     background-color: #acade9;
+}
+
+.footer-links {
+    list-style: none;
 }
 </style>
