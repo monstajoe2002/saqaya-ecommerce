9943742d82b51af1c131e096ceca522a10d5f265
monstajoe2002 <youssef.amr90@gmail.com>
2025-06-24 18:45:51 +0300

    add type definitions for vuex

--- vue.d.ts
+++ vue.d.ts
@@ -1,16 +1,1 @@
-// vuex.d.ts
-import { Store } from 'vuex'
-import { Product } from './src/types/product'
-declare module 'vue' {
-  // declare your own store states
-  interface State {
-    // cartItems: Array<CartItem>
-    products: Array<Product>
-    selectedProduct: Product | null
-  }
-
-  // provide typings for `this.$store`
-  interface ComponentCustomProperties {
-    $store: Store<State>
-  }
-}
+declare module 'vuex'
