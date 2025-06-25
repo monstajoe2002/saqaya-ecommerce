3360a9dfbb742508a0cace218cd0b0a971daa616
monstajoe2002 <youssef.amr90@gmail.com>
2025-06-25 17:00:37 +0300

    chore: create base card style

--- src/components/products/ProductCard.vue
+++ src/components/products/ProductCard.vue
@@ -11,8 +11,17 @@ export default {
 </script>
 
 <template>
-    <h1>{{ title }}</h1>
-    <img :src="image" :alt="title" />
-    <p>{{ price }}</p>
-    <span>{{ category }}</span>
+    <article class="product-card">
+        <h1>{{ title }}</h1>
+        <img :src="image" :alt="title" />
+        <p>{{ price }}</p>
+        <span>{{ category }}</span>
+    </article>
 </template>
+
+<style scoped lang="sass">
+.product-card
+    border: solid #000 thin;
+    border-radius: .75rem;
+    padding: .5rem;
+</style>
