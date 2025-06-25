a5877554fca3cc88bf8fd736a9ec40a28a178bda
monstajoe2002 <youssef.amr90@gmail.com>
2025-06-25 17:39:56 +0300

    chore: add a badge style to category

--- src/components/products/ProductCard.vue
+++ src/components/products/ProductCard.vue
@@ -15,17 +15,25 @@ export default {
         <h3>{{ title }}</h3>
         <img :src="image" :alt="title" class="product-card__image" />
         <p>{{ price }}</p>
-        <span>{{ category }}</span>
+        <span class="product-card__category">{{ category }}</span>
     </article>
 </template>
 
 <style scoped lang="sass">
 .product-card
-    border: solid #000 thin;
-    border-radius: .75rem;
-    padding: .5rem;
+    border: solid #000 thin
+    border-radius: .75rem
+    padding: .5rem
     &__image
-        max-width: 200px;
-        max-height: 400px;
-        object-fit: cover;
+        max-width: 200px
+        max-height: 400px
+        object-fit: cover
+    &__category
+        font-weight: 500
+        font-size: small
+        background-color:#acade9
+        border: solid #999 thin
+        margin-top: 8px
+        padding: 4px 8px
+        border-radius: 1rem
 </style>
