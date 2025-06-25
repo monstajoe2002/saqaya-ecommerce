fcc22241e3c09a8cf2af0408176e8c997f39723b
monstajoe2002 <youssef.amr90@gmail.com>
2025-06-25 18:47:00 +0300

    feat: register cart items module into store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,16 @@
 import { createStore } from 'vuex'
 import { productsModule } from './modules/products'
 import type { Product } from '@/types/product'
+import type { CartItem } from '@/types/cart-item'
+import { cartItemsModule } from './modules/cart-items'
 export interface State {
-  // cartItems: Array<CartItem>
+  cartItems: Array<CartItem>
   products: Array<Product>
   selectedProduct: Product | null
 }
 export const store = createStore({
   modules: {
     products: productsModule,
+    cartItems: cartItemsModule,
   },
 })
