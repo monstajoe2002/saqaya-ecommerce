e747adbe5197ade92ceb50b44298685ef677de7b
monstajoe2002 <youssef.amr90@gmail.com>
2025-06-27 14:33:01 +0300

    refactor: simplify SortDropdown component by removing FontAwesomeIcon

--- src/components/products/SortDropdown.vue
+++ src/components/products/SortDropdown.vue
@@ -1,26 +1,23 @@
 <script lang="ts">
-import { faSort } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-
 export default {
     name: 'SortDropdown',
-    components: {
-        FontAwesomeIcon
-    },
-    data() {
-        return {
-            faSort
-        }
-    }
 }
 </script>
 <template>
-    <label for="sort">
-        <FontAwesomeIcon :icon="faSort" /> Sort by
-    </label>
+    <div class="sort-dropdown">
+        <label for="sort">
+            <span>Sort by</span>
+        </label>
 
-    <select id="cars">
-        <option value="price">Price</option>
-        <option value="category">Category</option>
-    </select>
+        <select id="cars">
+            <option value="price">Price</option>
+            <option value="category">Category</option>
+        </select>
+    </div>
 </template>
+
+<style scoped lang="sass">
+// this will make sure the dropdown doesn't stretch to full width
+.sort-dropdown
+    max-width: fit-content
+</style>
