b54d8e9c63d0fe6734d7a058c4bff26a0a2975f8
monstajoe2002 <youssef.amr90@gmail.com>
2025-07-02 17:46:29 +0300

    feat: add typings for $route and $router

--- vuex.d.ts
+++ vuex.d.ts
@@ -2,6 +2,10 @@
 import { Store } from 'vuex'
 import type { State } from '@/store'
 declare module '@vue/runtime-core' {
+  interface ComponentCustomProperties {
+    $route: RouteLocationNormalizedLoaded
+    $router: Router
+  }
   // provide typings for `this.$store`
   interface ComponentCustomProperties {
     $store: Store<State>
