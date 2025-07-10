371ee6efbd5c697a86f0befcb1a2b91a4b7e25c3
monstajoe2002 <youssef.amr90@gmail.com>
2025-07-10 12:19:04 +0300

    refactor cart tests to use pinia

--- src/__tests__/Cart.test.ts
+++ src/__tests__/Cart.test.ts
@@ -1,8 +1,13 @@
 import { mount } from '@vue/test-utils'
 import { describe, expect, test, vi } from 'vitest'
 import SideCart from '../components/cart/SideCart.vue'
-
+import { useCartItems } from '../store/cart-items'
+import { createTestingPinia } from '@pinia/testing'
 describe('Shopping Cart', () => {
+  const pinia = createTestingPinia({
+    createSpy: vi.fn,
+    stubActions: false,
+  })
   test('Cart is empty initially', () => {
     const wrapper = mount(SideCart, {
       props: {
@@ -10,15 +15,14 @@ describe('Shopping Cart', () => {
       },
 
       global: {
-        mocks: {
-          $store: {
-            getters: {
-              getCartItems: vi.fn(() => []), // Mocking the getter to return an empty array
-            },
-          },
+        plugins: [pinia],
+        stubs: {
+          RouterLink: true,
         },
       },
     })
+    const cartItemsStore = useCartItems()
+    cartItemsStore.cartItems = []
     expect(wrapper.find('.cart__empty-fallback').text()).toContain('empty') // Adjust if your empty message differs
   })
   test('Cart displays items when not empty', () => {
@@ -38,44 +42,33 @@ describe('Shopping Cart', () => {
         quantity: 1,
       },
     ]
-    const mockTotalPrice = mockItems.reduce((acc, item) => acc + item.price * item.quantity, 0)
-    const mockTotalQuantity = mockItems.reduce((acc, item) => acc + item.quantity, 0)
+    const cartItemsStore = useCartItems(pinia)
+    cartItemsStore.cartItems = mockItems
+
     const wrapper = mount(SideCart, {
       props: {
         isOpen: true,
       },
-
       global: {
-        mocks: {
-          $store: {
-            getters: {
-              getCartItems: mockItems, // Mocking the getter to return mock items
-              getCartTotalPrice: mockTotalPrice,
-              getCartTotalQuantity: mockTotalQuantity,
-            },
-          },
+        plugins: [pinia],
+        stubs: {
+          RouterLink: true,
         },
       },
     })
+    console.log(wrapper.html())
 
-    // expect(wrapper.findAll('.cart__item').length).toBe(mockItems.length) // Check if the number of cart items matches
-    expect(wrapper.find('.cart__footer').text()).toContain(mockTotalPrice) // Check if the total price is calculated correctly
-    expect(wrapper.find('.cart__footer').text()).toContain(mockTotalQuantity) // Check if the total quantity is calculated correctly
+    expect(wrapper.findAll('.cart__item').length).toBe(mockItems.length) // Check if the number of cart items matches
+    expect(wrapper.find('.cart__footer').text()).toContain(cartItemsStore.getCartTotalPrice) // Check if the total price is calculated correctly
+    expect(wrapper.find('.cart__footer').text()).toContain(cartItemsStore.getCartTotalQuantity) // Check if the total quantity is calculated correctly
   })
   test('Cart emits toggle event when closed', async () => {
     const wrapper = mount(SideCart, {
       props: {
         isOpen: true,
       },
-
       global: {
-        mocks: {
-          $store: {
-            getters: {
-              getCartItems: vi.fn(() => []), // Mocking the getter to return an empty array
-            },
-          },
-        },
+        plugins: [pinia],
       },
     })
 
