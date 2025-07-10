4adffd4f1a6d628016c5495c421e148c2214ce4a
monstajoe2002 <youssef.amr90@gmail.com>
2025-07-10 18:03:12 +0300

    remove index.ts from store folder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { store } from './store'
 import { createPinia } from 'pinia'
 
 const app = createApp(App)
@@ -9,5 +8,3 @@ const pinia = createPinia()
 app.use(router)
 app.use(pinia)
 app.mount('#app')
-
-app.use(store)
